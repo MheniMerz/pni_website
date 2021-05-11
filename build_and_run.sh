f131d71115e4df98014e1c35216dbd45ed583d73
mhenimerz <hani.merzouki@gmail.com>
2021-05-11 09:32:22 +0000

    make container persistent after reboot

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,15 +2,13 @@
 
 # check if docker is installed
 # if not install it
-
 [[ -x "$(command -v docker)" ]] || sudo yum install docker docker.io -y
 
 # if you are on Ubuntu comment the previous line 
 # and use the the following line instead
-
 # [[ -x "$(command -v docker)" ]] || sudo apt install docker docker.io -y
 
 sudo systemctl start docker.service
 
 sudo docker build . -t pni_website
-sudo docker run -d -p 80:80  --name pni_website pni_website
+sudo docker run -d -p 80:80 --name pni_website --restart unless-stopped pni_website
